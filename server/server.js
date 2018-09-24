ba1d6a630c93e5051c16e5fbb828e992946e9cc2
Liron193 <liron@probot.ai>
2018-09-24 15:36:32 +0300

    Added GET /todos/id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./DB/mongoose-conf');
 var {ToDo} = require('./Models/todo');
@@ -38,6 +39,28 @@ app.get('/todos', (req, res) => {
             res.status(400).send(err);
         })
 });
+
+// Getting todo by id
+app.get('/todos/:id', (req, res) => {
+        var id = req.params.id
+
+        // Valid id
+        if(!ObjectID.isValid(id)){
+            res.status(404).send("The ID is invalid");
+        } else {
+            ToDo.findById(id)
+                .then((todo) => {
+                    if(!todo){
+                        res.status(404).send("There is no any TODO with that ID");
+                    } else {
+                        res.status(200).send({todo});
+                    }
+                })
+                .catch((err) => {
+                    res.status(400).send();
+                });
+        }
+    })
 
 app.listen(3000, () => {
     console.log('Started app at 3000');
