a3c4ec6c4fd70b0578e9253efbe463f5665469a6
Liron193 <liron@probot.ai>
2018-09-27 18:34:47 +0300

    Update config to use config.json file

--- server/Models/user.js
+++ server/Models/user.js
@@ -4,8 +4,6 @@ const jwt = require('jsonwebtoken');
 const _ = require('lodash');
 const bcrypt = require('bcryptjs');
 
-const secret = 'abc123';
-
 var UserSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -50,7 +48,7 @@ UserSchema.methods.toJSON = function () {
 UserSchema.methods.generateAuthToken = function () {
     var user = this;
     var access = 'auth';
-    var token = jwt.sign({_id: user._id.toHexString(), access}, secret).toString();
+    var token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString();
 
     user.tokens.push({access, token});
 
@@ -79,7 +77,7 @@ UserSchema.statics.findByToken = function (token) {
     var decoded;
 
     try{
-        decoded = jwt.verify(token, secret);
+        decoded = jwt.verify(token, process.env.JWT_SECRET);
     } catch(e){
         return Promise.reject();
     }
